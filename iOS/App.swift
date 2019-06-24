3ca1353400cc637f2bb68366ee121f89551bd1c5
vauxhall <zero.griffin@gmail.com>
2019-06-24 13:52:01 +0200

    Configure project

---
+++ iOS/App.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+private(set) weak var app: App!
+
+@UIApplicationMain final class App: UIViewController, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_: UIApplication, didFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        app = self
+        let window = UIWindow()
+        window.rootViewController = self
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
