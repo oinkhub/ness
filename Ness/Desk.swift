f16381030ce4cacfe73df2fb8f3718447c565708
vauxhall <zero.griffin@gmail.com>
2019-06-26 07:15:49 +0200

    Remove page

--- Ness/Desk.swift
+++ Ness/Desk.swift
@@ -1,14 +1,17 @@
 import Foundation
 
 final public class Desk {
-    internal(set) var page = Page()
+    public var content = "" { didSet { changed = true } }
+    private(set) var changed = false
+    private var url: URL?
     private let dispatch = Dispatch()
     
     public init() { }
     
     public func open(_ error: @escaping((Error) -> Void), _ next: @escaping(() -> Void)) {
         dispatch.background({ [weak self] in
-            if self?.page.status == .changed {
+            guard let self = self else { return }
+            if self.changed && self.url == nil {
                 throw Errorness("Page not saved")
             }
         }, error, next)
--- Tests/TestDesk.swift
+++ Tests/TestDesk.swift
@@ -8,6 +8,12 @@ final class TestDesk: XCTestCase {
         desk = Desk()
     }
     
+    func testUpdateContent() {
+        XCTAssertFalse(desk.changed)
+        desk.content = "hello world"
+        XCTAssertTrue(desk.changed)
+    }
+    
     func testOpenEmpty() {
         let expect = expectation(description: "")
         DispatchQueue.global(qos: .background).async {
@@ -21,7 +27,7 @@ final class TestDesk: XCTestCase {
     
     func testOpenChanged() {
         let expect = expectation(description: "")
-        desk.page.content = "hello world"
+        desk.content = "hello world"
         DispatchQueue.global(qos: .background).async {
             self.desk.open({ _ in
                 XCTAssertEqual(.main, Thread.current)
